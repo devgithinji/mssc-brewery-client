05fa0f583a1cd01888b24b5be2440f2adbee7040
dennis githinji <wakahiad@gmail.com>
2023-05-21 16:16:58 +0300

    http put

--- src/test/java/guru/springframework/msscbreweryclient/web/client/BreweryClientTest.java
+++ src/test/java/guru/springframework/msscbreweryclient/web/client/BreweryClientTest.java
@@ -28,4 +28,10 @@ class BreweryClientTest {
         assertNotNull(uri);
         System.out.println(uri.toString());
     }
+
+    @Test
+    void updateBeer() {
+        BeerDto beerDto = BeerDto.builder().beerName("New Beer").build();
+        breweryClient.updateBeer(UUID.randomUUID(), beerDto);
+    }
 }
