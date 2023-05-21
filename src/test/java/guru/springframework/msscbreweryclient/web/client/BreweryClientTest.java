43619d7104505f54aaac8f073e6fb407e2c5ba7a
dennis githinji <wakahiad@gmail.com>
2023-05-21 16:11:56 +0300

    http post

--- src/test/java/guru/springframework/msscbreweryclient/web/client/BreweryClientTest.java
+++ src/test/java/guru/springframework/msscbreweryclient/web/client/BreweryClientTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.net.URI;
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
@@ -18,5 +19,13 @@ class BreweryClientTest {
     void getBeerById() {
         BeerDto beerDto = breweryClient.getBeerById(UUID.randomUUID());
         assertNotNull(beerDto);
+    }
+
+    @Test
+    void savedBeer() {
+        BeerDto beerDto = BeerDto.builder().beerName("New Beer").build();
+        URI uri = breweryClient.savedBeer(beerDto);
+        assertNotNull(uri);
+        System.out.println(uri.toString());
     }
 }
